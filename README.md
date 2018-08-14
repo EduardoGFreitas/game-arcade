d1a7f7cd4d559368793a689c78651d8d973d600d
Eduardo Freitas <egoncalvesfreitas@gmail.com>
2018-08-14 20:48:32 -0300

    Alteração no texto do readme e correções de bugs

--- README.md
+++ README.md
@@ -4,11 +4,13 @@
 
 Jogo do tipo arcade baseado no  [frogger](http://froggerclassic.appspot.com/).
 
-## Pontuação
+## Rodando o jogo
+Baixe o projeto e abra o arquivo index com o browser de sua escolha.
+
+##Pontuação
 Ganha-se um ponto a cada vez que você conseguir atravessar a rua.
 
 ## Como Jogar
-
 Leve o seu personagem até o outro lado da avenida utilizando as teclas direcionais 
 
 ## Desafio
--- js/app.js
+++ js/app.js
@@ -1,80 +1,60 @@
-//let rows = [83 * 1 - 20, 83 * 2 - 20, 83 * 3 - 20];
 
-// Enemies our player must avoid
-class Enemy {
-    constructor(x, y, speed) {
-        this._x =x;
+
+class Character {
+    constructor(x, y, speed, sprite) {
+        this._x = x;
         this._y = y;
         this._speed = speed;
 
-        this._sprite = 'images/enemy-bug.png';
+        this._sprite = sprite;
     }
 
+    update() {
+        throw new Error('The method must be implemented');
+    }
+    render() {
+        ctx.drawImage(Resources.get(this._sprite), this._x, this._y)
+    }
+
+}
+class Enemy extends Character {
+
+    
     update(dt) {
-       
+
         this._x = this._x + this._speed * dt;
         if (this._x > 550) {
             this._x = -100;
             this._speed = 100 + Math.floor(Math.random() * 512);
         }
-       
-        this.checkCollision();
-    }
 
-    render() {
-        ctx.drawImage(Resources.get(this._sprite), this._x, this._y)
+        this.checkCollision(player);
     }
-    checkCollision(){
-        if (player._y + 131 >= this._y + 90 &&
-            player._y + 73 <= this._y + 135 &&
-            player._x + 25 <= this._x + 88 &&
-            player._x + 76 >= this._x + 11) {
-            console.log('collision');
-            gameReset();
+    checkCollision(jogador){
+        
+        if (jogador._y + 131 >= this._y + 90 &&
+            jogador._y + 73 <= this._y + 135 &&
+            jogador._x + 25 <= this._x + 88 &&
+            jogador._x + 76 >= this._x + 11) {
+        console.log('collision');
+        gameReset();
         }
     }
+
 }
 
-
-// var Enemy = function() {
-//     // Variables applied to each of our instances go here,
-//     // we've provided one for you to get started
-
-//     // The image/sprite for our enemies, this uses
-//     // a helper we've provided to easily load images
-//     this.sprite = 'images/enemy-bug.png';
-// };
-
-// // Update the enemy's position, required method for game
-// // Parameter: dt, a time delta between ticks
-// Enemy.prototype.update = function(dt) {
-//     // You should multiply any movement by the dt parameter
-//     // which will ensure the game runs at the same speed for
-//     // all computers.
-// };
-
-// // Draw the enemy on the screen, required method for game
-// Enemy.prototype.render = function() {
-//     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-// };
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-class Player {
-    constructor(x, y, speed) {
-        this._x = x;
-        this._y = y;
-        this._speed = speed
-        this._sprite = 'images/char-boy.png';
-    }
+class Player extends Character {
 
     update() {
-       
+
         this._x = this._x;
         this._y = this._y;
-      }
+    }
+
+    reset() {
+        this._x = 202.5;
+        this._y = 383;
+    }
 
     handleInput(key) {
         if (this._y <= (83 - 48)) {
@@ -83,26 +63,16 @@ class Player {
             return;
         }
 
-        key ==='left'&& this._x > 0 ? this._x -= this._speed + 50 : 0 ;
-        key ==='right'&& this._x < 400  ? this._x += this._speed + 50 : 0 ;
-        key ==='down'&& this._y < 380  ? this._y += this._speed + 30 :0;
-        key ==='up' && this._y > -20  ?  this._y -= this._speed + 30 : 0 ;
-    }
-    reset(){
-        this._x = 202.5;
-        this._y = 383;
+        key === 'left' && this._x > 50 ? this._x -= this._speed + 50 : 0;
+        key === 'right' && this._x < 400 ? this._x += this._speed + 50 : 0;
+        key === 'down' && this._y < 380 ? this._y += this._speed + 30 : 0;
+        key === 'up' && this._y > -20 ? this._y -= this._speed + 30 : 0;
     }
 
-    render() {
-        ctx.drawImage(Resources.get(this._sprite), this._x, this._y)
-    }
 }
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
 var score = 0;
-var player = new Player(200, 380, 50);
+var player = new Player(200, 380, 50, 'images/char-boy.png');
 var allEnemies = [];
 var scoreDiv = document.createElement('div');
 var canvasDiv = document.getElementsByTagName('canvas')[0];
@@ -116,9 +86,9 @@ function gameReset() {
     updateDisplay();
     allEnemies = [];
     allEnemies.push(
-        new Enemy(0, Math.random() * 150 + 50, Math.random() * 100 + 40),
-        new Enemy(0, Math.random() * 150 + 70, Math.random() * 100 + 60),
-        new Enemy(0, Math.random() * 160 + 50, Math.random() * 90 + 70)
+        new Enemy(0, Math.random() * 150 + 50, Math.random() * 100 + 40, 'images/enemy-bug.png'),
+        new Enemy(0, Math.random() * 150 + 70, Math.random() * 100 + 60, 'images/enemy-bug.png'),
+        new Enemy(0, Math.random() * 160 + 50, Math.random() * 90 + 70, 'images/enemy-bug.png')
     );
 }
 
@@ -127,7 +97,7 @@ function won() {
     score += 1;
     updateDisplay();
     if (score % 2 == 0 && allEnemies.length < 4) {
-        allEnemies.push(new Enemy(0, Math.random() * 160 + 50, Math.random() * 90 + 70));
+        allEnemies.push(new Enemy(0, Math.random() * 160 + 50, Math.random() * 90 + 70, 'images/enemy-bug.png'));
     }
 }
 
